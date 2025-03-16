7d60c621534ea89e8bb5391711975ed0135a3520
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-03-16 15:45:56 +0800

    Removed Input namespace

--- p2-mmorpg/Input.cs
+++ p2-mmorpg/Input.cs
@@ -1,25 +1,23 @@
-namespace Input
+abstract class IntInput
 {
-    abstract class IntInput
-    {
-        public abstract uint Value { get; set; }
-        public virtual uint ParseValue(string arg);
-    }
+public abstract uint Value { get; set; }
+public abstract uint ParseValue(string arg);
+}
 
-    class MaxInstances : IntInput
-    {
-        override public uint Value { get; set; } = 5;
-        override public uint ParseValue(string arg)
+class MaxInstances : IntInput
+{
+override public uint Value { get; set; } = 5;
+public override uint ParseValue(string arg)
+{
+        try
         {
-            try
-            {
-                Value = uint.Parse(arg);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error! ${e.Message}");
-                Console.WriteLine($"Setting max number of instances to {Value}");
-            }
+            Value = uint.Parse(arg);
         }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error! ${e.Message}");
+            Console.WriteLine($"Setting max number of instances to {Value}");
+        }
+        return Value;
     }
 }
