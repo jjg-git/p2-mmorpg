830b92044ad68b4a31eb79014a26deef5a4f1eef
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-03-16 15:57:14 +0800

    Added Party class in Program.cs

--- p2-mmorpg/Program.cs
+++ p2-mmorpg/Program.cs
@@ -6,3 +6,63 @@ uint dps = 1;
 
 maxInstances = (new Input.MaxInstances()).Parse(args[0]);
 
+class Party
+{
+
+    public bool addTanks()
+    {
+        if (tanks == maxTanks)
+            return false;
+
+        tanks += 1;
+        return true;
+    }
+
+    public bool addHealer()
+    {
+        if (healer == maxHealer)
+            return false;
+
+        healer += 1;
+        return true;
+    }
+
+    public bool addDPS()
+    {
+        if (dps == maxDPS)
+            return false;
+        dps += 1;
+        return true;
+    }
+
+    public bool isFull()
+    {
+        return tanks == maxTanks &&
+               healer == maxHealer &&
+               dps == maxDPS;
+    }
+
+    public void showInfo()
+    {
+        Console.WriteLine(
+            $"Party ID {id} {{tanks: {tanks}, healer: {healer}, dps: {dps}}}"
+        );
+    }
+
+    public Party()
+    {
+        id = count;
+        count++;
+    }
+
+    private const int maxTanks = 1;
+    private const int maxHealer = 1;
+    private const int maxDPS = 3;
+
+    private int tanks = 0;
+    private int healer = 0;
+    private int dps = 0;
+
+    private static int count = 0;
+    private int id = 0;
+}
