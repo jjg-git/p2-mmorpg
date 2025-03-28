5f4b7befc9adc12bf7bc6e7dcf5f03addb3da88c
jjg-git <john_joseph_giron@dlsu.edu.ph>
2025-03-28 13:35:16 +0800

    Added square braces for easy look up

--- test_run.sh
+++ test_run.sh
@@ -8,35 +8,35 @@ echo ""
 echo "--Input validation--"
 sleep 1s
 
-echo "$testcase: All zeros"
+echo "[$testcase]: All zeros"
 sleep 1s
 echo "dotnet run 0 0 0 0 0 0"
 dotnet run 0 0 0 0 0 0
 ((testcase+=1))
 echo ""
 
-echo "$testcase: All zeros except minTime and maxTime"
+echo "[$testcase]: All zeros except minTime and maxTime"
 sleep 1s
 echo "dotnet run 0 0 0 0 1 2"
 dotnet run 0 0 0 0 0 0
 ((testcase+=1))
 echo ""
 
-echo "$testcase: Negative 1s"
+echo "[$testcase]: Negative 1s"
 sleep 1s
 echo "dotnet run -1 -1 -1 -1 -1 -1"
 dotnet run 0 0 0 0 0 0
 ((testcase+=1))
 echo ""
 
-echo "$testcase: Negative Max value"
+echo "[$testcase]: Negative Max value"
 sleep 1s
-echo "dotnet run -65535 -65535 -65535 -65535 0 0"
-dotnet run -65535 -65535 -65535 -65535 0 0
+echo "dotnet run -65535 -65535 -65535 -65535 -65535 -65535"
+dotnet run -65535 -65535 -65535 -65535 -65535 -65535
 ((testcase+=1))
 echo ""
 
-echo "$testcase: Positive Max value"
+echo "[$testcase]: Positive Max value"
 sleep 1s
 echo "dotnet run 65535 65535 65535 65535 0 15"
 dotnet run 65535 65535 65535 65535 0 15
@@ -61,68 +61,70 @@ echo ""
 echo "--Program specifics--"
 sleep 1s
 
-echo "$testcase: One instance, no party"
+echo "[$testcase]: One instance, no party"
 sleep 1s
+echo "dotnet run 1 0 0 0 1 2"
+dotnet run 1 0 0 0 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: One party, no instance"
+echo "[$testcase]: One party, no instance"
 sleep 1s
 echo "dotnet run 0 1 1 3 1 2"
 dotnet run 0 1 1 3 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: One party, no instance with excess members"
+echo "[$testcase]: One party, no instance with excess members"
 sleep 1s
 echo "dotnet run 0 1 1 6 1 2"
 dotnet run 0 1 1 6 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: One party, one instance"
+echo "[$testcase]: One party, one instance"
 sleep 1s
 echo "dotnet run 1 1 1 3 1 2"
 dotnet run 1 1 1 3 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: One party, one instance, with excess members"
+echo "[$testcase]: One party, one instance, with excess members"
 sleep 1s
 echo "dotnet run 1 1 1 6 1 2"
 dotnet run 1 1 1 6 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: Many instances, one party"
+echo "[$testcase]: Many instances, one party"
 sleep 1s
 echo "dotnet run 50 1 1 3 1 2"
 dotnet run 50 1 1 3 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: Many parties, one instance"
+echo "[$testcase]: Many parties, one instance"
 sleep 1s
 echo "dotnet run 1 50 50 150 1 2"
 dotnet run 1 50 50 150 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: Many parties, one instance, with excess members"
+echo "[$testcase]: Many parties, one instance, with excess members"
 sleep 1s
 echo "dotnet run 1 50 60 200 1 2"
 dotnet run 1 50 60 300 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: Many parties, many instances"
+echo "[$testcase]: Many parties, many instances"
 sleep 1s
 echo "dotnet run 50 50 50 150 1 2"
 dotnet run 50 50 50 150 1 2
 ((testcase+=1))
 echo ""
 
-echo "$testcase: Many parties, many instances, with excess members"
+echo "[$testcase]: Many parties, many instances, with excess members"
 sleep 1s
 echo "dotnet run 50 50 60 200 1 2"
 dotnet run 50 50 60 300 1 2
